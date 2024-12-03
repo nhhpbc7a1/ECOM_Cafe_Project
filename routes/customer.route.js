f306f141a054c7f5f15b9f5f75d0f9e9bb496c48
Nguyễn Hoàng Huy <114816906+nhhpbc7a1@users.noreply.github.com>
2024-12-03 22:12:25 +0700

    haha

--- routes/customer.route.js
+++ routes/customer.route.js
@@ -3,6 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 import menuRouter from './customer/menu.route.js';
+import createOrderService from '../services/createOrder.service.js'
 router.use('/menu', menuRouter);
 
 router.get('/cart', async function (req, res) {
@@ -12,7 +13,40 @@ router.get('/cart', async function (req, res) {
     });
 });
 router.post('/cart', async function (req, res) {
-    req
+    
+    if (req.session.cart == undefined) {
+        res.status(404);
+    }
+    const cart = req.session.cart;
+
+    let order = {
+        table_id: req.session.table_id,
+        order_date: new Date(),
+        status: 'Pending',
+        total_amount: 50,
+    }
+
+    const newOrder_id = await createOrderService.add_order(order);
+
+    for(let item of cart) {
+        const order_item = {
+            order_id: newOrder_id,
+            menu_item_id: item.product_id,
+            quantity: item.quantity,
+            note: "",
+        }
+        const newOrder_item_id = await createOrderService.add_order_item(order_item);
+
+        for (let topping of item.toppings) {
+            const order_item_topping = {
+                order_item_id: newOrder_item_id,
+                topping_id: topping.topping_id,
+            }
+            await createOrderService.add_order_item_topping(order_item_topping);
+        }
+    }
+    console.log("heellasdasdasdasdasd")
+    console.log(cart);
     res.render('vwCustomer/cart.hbs', {
         layout: false,
     });
